c994cd65cf4f6ce2e9ec0223cae8f8102440ce4c
Mesut <semihturgutlu@gmail.com>
2023-12-02 23:25:12 +0100

    navbar section

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,27 +1,47 @@
-import React from 'react'
-import "./navbar.scss"
-import SearchIcon from '@mui/icons-material/Search';
+import React from "react";
+import "./navbar.scss";
+import SearchIcon from "@mui/icons-material/Search";
+import NotificationsIcon from "@mui/icons-material/NotificationsNone";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 
 const Navbar = () => {
   return (
-    <div className='navbar'>
-        <div className="container">
-            <div className="left">
-                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Netflix_logo.svg/800px-Netflix_logo.svg.png" alt="" /> 
-                <span>Homepage</span>
-                <span>Series</span>
-                <span>Movies</span>
-                <span>New und Popular</span>
-                <span>My List</span>
-                
-                </div>
-            <div className="right">
-                <SearchIcon/>
-
-            </div>
+    <div className="navbar">
+      <div className="container">
+        <div className="left">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Netflix_logo.svg/800px-Netflix_logo.svg.png"
+            alt=""
+          />
+          <span>Homepage</span>
+          <span>Series</span>
+          <span>Movies</span>
+          <span>New und Popular</span>
+          <span>My List</span>
         </div>
-    </div>
-  )
-}
+        <div className="right">
+          <SearchIcon className="icon" />
+          <span>KID</span>
+          <NotificationsIcon className="icon" />
+          <img
+            src="https://images.pexels.com/photos/19296340/pexels-photo-19296340.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load"
+            alt=""
+          />
+          <div className="profile">
+            <ArrowDropDownIcon className="icon" />
+            <div className="options">
+              <span>Settings</span>
+              <span>logout</span>
+            </div>
+          </div>
+        </div>
+      </div>
 
-export default Navbar
+      <div className="left"></div>
+
+      <div className="rigt"></div>
+    </div>
+  );
+};
+
+export default Navbar;
--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -7,6 +7,50 @@ const Home = () => {
   return (
     <div className='home'>
       < Navbar/>
+      <img
+            src="https://images.pexels.com/photos/19296340/pexels-photo-19296340.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load"
+            alt=""
+            width="100%"
+          />
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio nihil beatae officiis, veritatis ducimus sapiente fugit ipsam repellat, voluptate voluptas magni similique laboriosam doloremque officia fugiat quis pariatur deleniti eum.
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio nihil beatae officiis, veritatis ducimus sapiente fugit ipsam repellat, voluptate voluptas magni similique laboriosam doloremque officia fugiat quis pariatur deleniti eum.
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio nihil beatae officiis, veritatis ducimus sapiente fugit ipsam repellat, voluptate voluptas magni similique laboriosam doloremque officia fugiat quis pariatur deleniti eum.
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio nihil beatae officiis, veritatis ducimus sapiente fugit ipsam repellat, voluptate voluptas magni similique laboriosam doloremque officia fugiat quis pariatur deleniti eum.
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio nihil beatae officiis, veritatis ducimus sapiente fugit ipsam repellat, voluptate voluptas magni similique laboriosam doloremque officia fugiat quis pariatur deleniti eum.
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio nihil beatae officiis, veritatis ducimus sapiente fugit ipsam repellat, voluptate voluptas magni similique laboriosam doloremque officia fugiat quis pariatur deleniti eum.
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio nihil beatae officiis, veritatis ducimus sapiente fugit ipsam repellat, voluptate voluptas magni similique laboriosam doloremque officia fugiat quis pariatur deleniti eum.
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio nihil beatae officiis, veritatis ducimus sapiente fugit ipsam repellat, voluptate voluptas magni similique laboriosam doloremque officia fugiat quis pariatur deleniti eum.
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio nihil beatae officiis, veritatis ducimus sapiente fugit ipsam repellat, voluptate voluptas magni similique laboriosam doloremque officia fugiat quis pariatur deleniti eum.
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio nihil beatae officiis, veritatis ducimus sapiente fugit ipsam repellat, voluptate voluptas magni similique laboriosam doloremque officia fugiat quis pariatur deleniti eum.
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio nihil beatae officiis, veritatis ducimus sapiente fugit ipsam repellat, voluptate voluptas magni similique laboriosam doloremque officia fugiat quis pariatur deleniti eum.
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio nihil beatae officiis, veritatis ducimus sapiente fugit ipsam repellat, voluptate voluptas magni similique laboriosam doloremque officia fugiat quis pariatur deleniti eum.
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio nihil beatae officiis, veritatis ducimus sapiente fugit ipsam repellat, voluptate voluptas magni similique laboriosam doloremque officia fugiat quis pariatur deleniti eum.
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio nihil beatae officiis, veritatis ducimus sapiente fugit ipsam repellat, voluptate voluptas magni similique laboriosam doloremque officia fugiat quis pariatur deleniti eum.
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio nihil beatae officiis, veritatis ducimus sapiente fugit ipsam repellat, voluptate voluptas magni similique laboriosam doloremque officia fugiat quis pariatur deleniti eum.
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio nihil beatae officiis, veritatis ducimus sapiente fugit ipsam repellat, voluptate voluptas magni similique laboriosam doloremque officia fugiat quis pariatur deleniti eum.
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio nihil beatae officiis, veritatis ducimus sapiente fugit ipsam repellat, voluptate voluptas magni similique laboriosam doloremque officia fugiat quis pariatur deleniti eum.
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio nihil beatae officiis, veritatis ducimus sapiente fugit ipsam repellat, voluptate voluptas magni similique laboriosam doloremque officia fugiat quis pariatur deleniti eum.
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio nihil beatae officiis, veritatis ducimus sapiente fugit ipsam repellat, voluptate voluptas magni similique laboriosam doloremque officia fugiat quis pariatur deleniti eum.
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio nihil beatae officiis, veritatis ducimus sapiente fugit ipsam repellat, voluptate voluptas magni similique laboriosam doloremque officia fugiat quis pariatur deleniti eum.
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio nihil beatae officiis, veritatis ducimus sapiente fugit ipsam repellat, voluptate voluptas magni similique laboriosam doloremque officia fugiat quis pariatur deleniti eum.
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio nihil beatae officiis, veritatis ducimus sapiente fugit ipsam repellat, voluptate voluptas magni similique laboriosam doloremque officia fugiat quis pariatur deleniti eum.
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio nihil beatae officiis, veritatis ducimus sapiente fugit ipsam repellat, voluptate voluptas magni similique laboriosam doloremque officia fugiat quis pariatur deleniti eum.
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio nihil beatae officiis, veritatis ducimus sapiente fugit ipsam repellat, voluptate voluptas magni similique laboriosam doloremque officia fugiat quis pariatur deleniti eum.
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio nihil beatae officiis, veritatis ducimus sapiente fugit ipsam repellat, voluptate voluptas magni similique laboriosam doloremque officia fugiat quis pariatur deleniti eum.
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio nihil beatae officiis, veritatis ducimus sapiente fugit ipsam repellat, voluptate voluptas magni similique laboriosam doloremque officia fugiat quis pariatur deleniti eum.
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio nihil beatae officiis, veritatis ducimus sapiente fugit ipsam repellat, voluptate voluptas magni similique laboriosam doloremque officia fugiat quis pariatur deleniti eum.
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio nihil beatae officiis, veritatis ducimus sapiente fugit ipsam repellat, voluptate voluptas magni similique laboriosam doloremque officia fugiat quis pariatur deleniti eum.
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio nihil beatae officiis, veritatis ducimus sapiente fugit ipsam repellat, voluptate voluptas magni similique laboriosam doloremque officia fugiat quis pariatur deleniti eum.
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio nihil beatae officiis, veritatis ducimus sapiente fugit ipsam repellat, voluptate voluptas magni similique laboriosam doloremque officia fugiat quis pariatur deleniti eum.
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio nihil beatae officiis, veritatis ducimus sapiente fugit ipsam repellat, voluptate voluptas magni similique laboriosam doloremque officia fugiat quis pariatur deleniti eum.
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio nihil beatae officiis, veritatis ducimus sapiente fugit ipsam repellat, voluptate voluptas magni similique laboriosam doloremque officia fugiat quis pariatur deleniti eum.
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio nihil beatae officiis, veritatis ducimus sapiente fugit ipsam repellat, voluptate voluptas magni similique laboriosam doloremque officia fugiat quis pariatur deleniti eum.
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio nihil beatae officiis, veritatis ducimus sapiente fugit ipsam repellat, voluptate voluptas magni similique laboriosam doloremque officia fugiat quis pariatur deleniti eum.
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio nihil beatae officiis, veritatis ducimus sapiente fugit ipsam repellat, voluptate voluptas magni similique laboriosam doloremque officia fugiat quis pariatur deleniti eum.
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio nihil beatae officiis, veritatis ducimus sapiente fugit ipsam repellat, voluptate voluptas magni similique laboriosam doloremque officia fugiat quis pariatur deleniti eum.
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio nihil beatae officiis, veritatis ducimus sapiente fugit ipsam repellat, voluptate voluptas magni similique laboriosam doloremque officia fugiat quis pariatur deleniti eum.
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio nihil beatae officiis, veritatis ducimus sapiente fugit ipsam repellat, voluptate voluptas magni similique laboriosam doloremque officia fugiat quis pariatur deleniti eum.
+
     </div>
   )
 }
