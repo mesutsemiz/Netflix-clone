3e33bb4542490f50f2b8db50b0fb2288fee83eb0
Mesut <semihturgutlu@gmail.com>
2023-12-03 21:55:52 +0100

    navbar is completed

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,10 +3,22 @@ import "./navbar.scss";
 import SearchIcon from "@mui/icons-material/Search";
 import NotificationsIcon from "@mui/icons-material/NotificationsNone";
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import { useState } from "react";
 
 const Navbar = () => {
+
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  window.onscroll = ()=>{
+    setIsScrolled(window.pageYOffset === 0 ? false : true);
+
+    return () => {
+      window.onscroll = null;
+    }
+  }
+
   return (
-    <div className="navbar">
+    <div className={isScrolled ? "navbar scrolled" : "navbar"}>
       <div className="container">
         <div className="left">
           <img
