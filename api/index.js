144460b5b24a5968f7f4fba2cfa6f857f9ee5d23
Mesut <semihturgutlu@gmail.com>
2023-12-08 23:55:16 +0100

    routes files is created

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const authRoute = require("./routes/auth") 
 
 dotenv.config()
 
@@ -11,6 +12,8 @@ mongoose.connect(process.env.MONGO_URL)
 }).catch((err)=>{
     console.log(err)
 });
+
+app.use("/api/auth", authRoute);
 
 app.listen(5500, () => {
   console.log("backend server is running");
