0c6ea4b3e4ce8283e9773e16175269f4121023d5
Mesut <semihturgutlu@gmail.com>
2023-12-10 17:42:51 +0100

    users root is completed

--- api/routes/users.js
+++ api/routes/users.js
@@ -62,6 +62,69 @@ router.get("/find/:id", async (req, res) => {
   } 
 );
 
+//GET ALL
+
+router.get("/", verify, async (req, res) => {
+
+  const query =req.query.new;
+
+  if ( req.user.isAdmin) {
+
+    try {
+      const users = query ? await User.find().sort({_id:-1}).limit(10) : await User.find();
+      res.status(200).json(users);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(403).json("You are not allowed to see all users!");
+  }
+});
+
+//GET USER STATS
+
+router.get("/stats", async (req,res)=>{
+
+  const today = new Date();
+  const lastYear = today.setFullYear(today.setFullYear()-1)
+
+  const monthsArray = [
+    "Januar",
+    "Februar",
+    "march",
+    "April",
+    "May",
+    "June",
+    "July",
+    "Augst",
+    "September",
+    "October",
+    "November",
+    "December"
+
+  ]
+
+  try {
+
+    const data = await User.aggregate([
+      {
+        $project : {
+          month:{$month : "$createdAt"}
+        },
+      },
+      {
+        $group:{
+          _id:"$month",
+          total:{$sum:1}
+        },
+      },
+    ])
+    res.status(200).json(data)
+  } catch (error) {
+    res.status(500).json(err)
+  }
+
+})
 
 
 
