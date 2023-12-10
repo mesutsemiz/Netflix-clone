7833dd5294afde21540452226c683afa056a7229
Mesut <semihturgutlu@gmail.com>
2023-12-10 16:02:13 +0100

    update method ıs done

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const authRoute = require("./routes/auth") 
+const usersRoute = require("./routes/users") 
 
 dotenv.config()
 
@@ -16,6 +17,7 @@ mongoose.connect(process.env.MONGO_URL)
 app.use(express.json());
 
 app.use("/api/auth", authRoute);
+app.use("/api/users", usersRoute);
 
 app.listen(5500, () => {
   console.log("backend server is running");
