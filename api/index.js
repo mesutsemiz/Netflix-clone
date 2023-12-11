258c9a3272924987b7d303e737a7ccb0cc4665cf
Mesut <semihturgutlu@gmail.com>
2023-12-11 16:40:09 +0100

    route of list is finished

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ const dotenv = require("dotenv");
 const authRoute = require("./routes/auth"); 
 const usersRoute = require("./routes/users");
 const moviesRoute = require("./routes/movies")
+const listsRoute = require("./routes/lists")
 
 dotenv.config()
 
@@ -20,6 +21,7 @@ app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", usersRoute);
 app.use("/api/movies", moviesRoute);
+app.use("/api/lists", listsRoute);
 
 app.listen(5500, () => {
   console.log("backend server is running");
