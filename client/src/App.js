9e5d002f83bd8e47088ef86c8717d77994bda3dc
Mesut <semihturgutlu@gmail.com>
2023-12-12 14:07:39 +0100

    routes added to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,39 @@
 import Home from "./pages/home/Home";
-import './app.scss'
+import "./app.scss";
 import Watch from "./pages/watch/Watch";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
-
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 
 function App() {
+
+  const user =false;
   return (
-   <Watch/>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          {user ? <Home /> : <Redirect to="/register"/>}
+        </Route>
+        <Route  path="/register">
+        {!user ? <Register /> : <Redirect to="/"/>}
+        </Route>
+        <Route path="/login">
+        {!user ? <Login /> : <Redirect to="/"/>}
+        </Route>
+        {user &&  <>
+        <Route path="/series">
+          <Home type="series" />
+        </Route>
+        <Route path="/movies">
+          <Home type="movies" />
+        </Route>
+        <Route path="/watch">
+          <Watch />
+        </Route> 
+        </>
+        }
+      </Switch>
+    </Router>
   );
 }
 
--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -4,6 +4,8 @@ import SearchIcon from "@mui/icons-material/Search";
 import NotificationsIcon from "@mui/icons-material/NotificationsNone";
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 import { useState } from "react";
+import { Link } from "react-router-dom/cjs/react-router-dom";
+
 
 const Navbar = () => {
 
@@ -25,9 +27,15 @@ const Navbar = () => {
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Netflix_logo.svg/800px-Netflix_logo.svg.png"
             alt=""
           />
+           <Link to="/" className="link">
           <span>Homepage</span>
+         </Link>
+         <Link to="/series" className="link">
           <span>Series</span>
+         </Link>
+         <Link to="/movies" className="link">
           <span>Movies</span>
+         </Link>
           <span>New und Popular</span>
           <span>My List</span>
         </div>
