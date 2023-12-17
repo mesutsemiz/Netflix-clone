7088ac35a62264a76a0373c68d4bc53d4a9bfaf1
Mesut <semihturgutlu@gmail.com>
2023-12-17 14:43:47 +0100

    user login is done

--- admin/src/context/authContext/apiCalls.js
+++ admin/src/context/authContext/apiCalls.js
@@ -1,10 +1,10 @@
-import axios from "axois";
+import axios from "axios";
 import { loginFailure, loginStart, loginSuccess } from "./AuthActions";
 
 export const login = async(user, dispatch)=>{
-    dispatch(loginStart);
+    dispatch(loginStart());
     try {
-        const res = axios.get("auth/login", user);
+        const res = axios.post("auth/login", user);
         dispatch(loginSuccess(res.data))
     } catch (error) {
         dispatch(loginFailure())
--- admin/src/pages/login/Login.jsx
+++ admin/src/pages/login/Login.jsx
@@ -1,24 +1,42 @@
-import React, { useState } from 'react'
-import "../login/login.css"
+import React, { useContext, useState } from "react";
+import "../login/login.css";
+import { AuthContext } from "../../context/authContext/AuthContext";
+import { login } from "../../context/authContext/apiCalls";
 
-const login = () => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const {}
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { isFetching, dispatch } = useContext(AuthContext);
 
-    const handleLogin = ()=>{
-        e.preventDefault();
-        dispatch
-    }
+  const handleLogin = (e) => {
+    e.preventDefault();
+    login({ email, password }, dispatch);
+  };
   return (
     <div className="login">
-        <form action="" className="loginForm">
-            <input type="text" placeholder='email' className="loginInput" onChange={(e)=>setEmail(e.target.value)}/>
-            <input type="text" placeholder='password' className="loginInput" onChange={(e)=>setPassword(e.target.value)} />
-            <button className='loginButton' onClick={handleLogin}>Login</button>
-        </form>
+      <form action="" className="loginForm">
+        <input
+          type="text"
+          placeholder="email"
+          className="loginInput"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          className="loginInput"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button 
+          className="loginButton"
+          onClick={handleLogin}
+          disabled={isFetching}
+          >
+          Login
+        </button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default login
+export default Login;
