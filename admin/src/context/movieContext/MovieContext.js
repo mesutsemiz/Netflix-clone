420561099e2baf1a73e6f260bbd55d11248fbf9c
Mesut <semihturgutlu@gmail.com>
2023-12-18 20:24:16 +0100

    minor editing

--- admin/src/context/movieContext/MovieContext.js
+++ admin/src/context/movieContext/MovieContext.js
@@ -1,18 +1,27 @@
 import MovieReducer from "./MovieReducer";
-import { createContext, useReducer } from 'react';
-
+import { createContext, useReducer } from "react";
 
 const INITIAL_STATE = {
-    user:JSON.parse(localStorage.getItem("user")) || null,
-    isFetching:false,
-    error:false
+  movies: [],
+  isFetching: false,
+  error: false,
 };
 
 export const MovieContext = createContext(INITIAL_STATE);
 
-export const MovieContextProvider =({children})=>{
-    const [state, dispatch] = useReducer(MovieReducer, INITIAL_STATE);
+export const MovieContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(MovieReducer, INITIAL_STATE);
 
-    return(
-        <MovieContext.Provider value={{movies:state.movies, isFetching:state.isFetching, error:state.error,dispatch}}>{children}</MovieContext.Provider>    )
-}
+  return (
+    <MovieContext.Provider
+      value={{
+        movies: state.movies,
+        isFetching: state.isFetching,
+        error: state.error,
+        dispatch,
+      }}
+    >
+      {children}
+    </MovieContext.Provider>
+  );
+};
