06f4f33fe77f18a3721d24e942e8c4293844a172
Mesut <semihturgutlu@gmail.com>
2023-12-19 15:37:19 +0100

    Movie Page is editted

--- admin/src/pages/productList/ProductList.jsx
+++ admin/src/pages/productList/ProductList.jsx
@@ -51,7 +51,7 @@ export default function ProductList() {
       renderCell: (params) => {
         return (
           <>
-            <Link to={{pathname:"/movie/" + params.row._id, movie:params.row}}>
+            <Link to={{pathname:"/product/" + params.row._id, movie:params.row}}>
               <button className="productListEdit">Edit</button>
             </Link>
             <DeleteOutline
