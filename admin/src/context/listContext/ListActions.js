a7ac4ab62506c506cda498721aba758897603920
Mesut <semihturgutlu@gmail.com>
2023-12-26 17:29:27 +0100

    list context is created

---
+++ admin/src/context/listContext/ListActions.js
@@ -0,0 +1,48 @@
+export const getListstart = () => ({
+  type: "GET_LISTS_START",
+});
+
+export const getListsSuccess = (lists) => ({
+  type: "GET_LISTS_SUCCESS",
+  payload: lists,
+});
+
+export const getListsFailure = () => ({
+  type: "GET_LISTS_FAILURE",
+});
+// export const createMovieStart = () => ({
+//     type: "CREATE_MOVIE_START",
+//   });
+
+//   export const createMovieSuccess = (movie) => ({
+//     type: "CREATE_MOVIE_SUCCESS",
+//     payload: movie,
+//   });
+
+//   export const createMovieFailure = () => ({
+//     type: "CREATE_MOVIE_FAILURE",
+//   });
+// export const updateMovieStart = () => ({
+//     type: "UPDATE_MOVIE_START",
+//   });
+
+//   export const updateMovieSuccess = (movie) => ({
+//     type: "UPDATE_MOVIE_SUCCESS",
+//     payload: movie,
+//   });
+
+//   export const updateMovieFailure = () => ({
+//     type: "UPDATE_MOVIE_FAILURE",
+//   });
+
+// export const deleteMovieStart = ()=>({
+//     type : "DELETE_MOVIE_START",
+// })
+
+// export const deleteMovieSuccess = ()=>({
+//     type : "DELETE_MOVIE_SUCCESS",
+// });
+
+// export const deleteMovieFailure = () =>({
+//     type : "DELETE_MOVIE_FAILURE",
+// });
---
+++ admin/src/context/listContext/apiCalls.js
@@ -0,0 +1,59 @@
+import {
+  getMoviesFailure,
+  getMoviesStart,
+  getMoviesSuccess,
+  deleteMovieStart,
+  deleteMovieSuccess,
+  deleteMovieFailure,
+  createMovieStart,
+  createMovieSuccess,
+  createMovieFailure
+} from "./MovieActions";
+import axios from "axios";
+
+export const getMovies = async (dispatch) => {
+  dispatch(getMoviesStart());
+  try {
+    const res = await axios.get("/movies", {
+      headers: {
+        token: "Bearer " + JSON.parse(localStorage.getItem("user")).accessToken,
+      },
+    });
+    dispatch(getMoviesSuccess(res.data));
+  } catch (err) {
+    dispatch(getMoviesFailure());
+  }
+};
+
+//create
+
+export const createMovie = async (movie,dispatch) => {
+  dispatch(createMovieStart());
+  try {
+    const res = await axios.post("/movies", movie, {
+      headers: {
+        token: "Bearer " + JSON.parse(localStorage.getItem("user")).accessToken,
+      },
+    });
+    dispatch(createMovieSuccess(res.data));
+  } catch (err) {
+    dispatch(createMovieFailure());
+  }
+};
+
+//delete 
+
+export const deleteMovie = async (id, dispatch) => {
+  dispatch(deleteMovieStart());
+  try {
+    const res = await axios.delete("/movies"+id, {
+      headers: {
+        token: "Bearer " + JSON.parse(localStorage.getItem("user")).accessToken,
+      },
+    });
+    dispatch(deleteMovieSuccess(id));
+  } catch (err) {
+    dispatch(deleteMovieFailure());
+  }
+};
+
